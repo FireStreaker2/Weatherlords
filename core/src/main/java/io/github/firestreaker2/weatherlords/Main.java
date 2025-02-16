c9eb0f468f36a652147c58d2d81e566fd6963eec
OPwinner <pattipakaabhinav@gmail.com>
2025-02-15 18:17:21 -0800

    Added movement with arrow keys and mouse click

--- core/src/main/java/io/github/firestreaker2/weatherlords/Main.java
+++ core/src/main/java/io/github/firestreaker2/weatherlords/Main.java
@@ -2,6 +2,7 @@ package io.github.firestreaker2.weatherlords;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,20 +10,28 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.math.Vector2;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
 public class Main extends InputAdapter implements ApplicationListener {
     Texture pfp;
+
+    Vector2 touchPos;
     SpriteBatch spriteBatch;
     FitViewport viewport;
+
+    Sprite guraSprite;
 
     @Override
     public void create() {
         Gdx.input.setInputProcessor(this);
 
+        touchPos = new Vector2();
         pfp = new Texture("pfp.png");
 
         spriteBatch = new SpriteBatch();
+        guraSprite = new Sprite(pfp);
+        guraSprite.setSize(1, 1);
         viewport = new FitViewport(8, 5);
     }
 
@@ -39,7 +48,26 @@ public class Main extends InputAdapter implements ApplicationListener {
     }
 
     private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            guraSprite.translateY(speed * delta);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            guraSprite.translateY(-speed * delta);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            guraSprite.translateX(-speed * delta);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            guraSprite.translateX(speed * delta);
+        }
+        if(Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(touchPos);
+            guraSprite.setCenter(touchPos.x, touchPos.y);
+        }
     }
 
     private void logic() {
@@ -52,7 +80,7 @@ public class Main extends InputAdapter implements ApplicationListener {
         spriteBatch.setProjectionMatrix((viewport.getCamera().combined));
         spriteBatch.begin();
 
-        spriteBatch.draw(pfp, 1, 1, 1, 1);
+        guraSprite.draw(spriteBatch);
 
         spriteBatch.end();
     }
